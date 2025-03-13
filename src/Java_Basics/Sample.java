be9cfa2940a98fb98657a83fc38d06adf09c4cdc
KadariGSVMohan <gsvmohankadari@gmail.com>
2025-03-13 08:18:00 +0530

    First One

---
+++ src/Java_Basics/Sample.java
@@ -0,0 +1,11 @@
+package Java_Basics;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mohan");
+		System.out.println("Mohan");
+	}
+
+}
