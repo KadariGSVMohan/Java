b00c69ac36b920f6c2bf893f386a2479b9635559
KadariGSVMohan <gsvmohankadari@gmail.com>
2025-03-15 19:06:53 +0530

    Sample

---
+++ src/Walk_in/CSE.java
@@ -0,0 +1,16 @@
+//Simple Class & Object Program
+
+
+package Walk_in;
+
+public class CSE {
+ int age = 20;
+ int m1 = 40;
+ int m2 = 30;
+ String name = "mohan";
+	public static void main(String[] args) {
+		CSE s1 = new CSE();
+		System.out.println(s1.name);
+	}
+
+}
