7881d84d732b9a8546202687eb2a3dc3152d4f8c
KadariGSVMohan <gsvmohankadari@gmail.com>
2025-03-15 21:38:38 +0530

    Secound_One

---
+++ src/Walk_in/Controll_Stats.java
@@ -0,0 +1,13 @@
+// Simple if Statement 
+
+package Walk_in;
+public class Controll_Stats {
+	public static void main(String[] args)
+	{
+		int a = 11;
+		if (a <12) 
+		{
+			System.out.println("Mohan");
+		}
+   }
+}
